cb7589154e52dec3b820cdd497bfbc86f10ce36a
Robert Knight <robertknight@gmail.com>
2022-09-19 07:39:05 +0100

    Update main.rs to load my test model

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 mod graph;
 mod model;
 mod ops;
@@ -15,12 +17,14 @@ use rng::XorShiftRNG;
 use tensor::random_tensor;
 
 fn main() {
-    let model_bytes = [0u8, 1u8, 2u8, 3u8];
-    let mut rng = XorShiftRNG::new(1234);
-
+    let model_bytes = fs::read("output.model").unwrap();
     let model = load_model(&model_bytes).unwrap();
-    let input_id = model.find_node("input").unwrap();
-    let output_id = model.find_node("output").unwrap();
-    let img_data = random_tensor(vec![800, 600, 1], &mut rng);
-    let _text_mask = model.run(&[(input_id, &img_data)], &[output_id]);
+    let input_id = model.find_node("input.1").unwrap();
+    let output_id = model.find_node("380").unwrap();
+
+    let mut rng = XorShiftRNG::new(1234);
+    let img_data = random_tensor(vec![1, 1, 800, 600], &mut rng);
+    let text_mask = model.run(&[(input_id, &img_data)], &[output_id]);
+
+    println!("Mask shape {:?}", text_mask[0].shape);
 }
