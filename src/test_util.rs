570a14762abd0731811a92a53ee9b47539a3d314
Robert Knight <robertknight@gmail.com>
2022-10-23 16:01:36 +0100

    Handle non-contiguous tensors in expect_equal
    Use `Tensor::elements` to iterate over elements in expect_equal so
    comparisons work if strides are different. Also change the error to be a
    summary of the mismatches instead of just the index and values of the
    first one.

--- src/test_util.rs
+++ src/test_util.rs
@@ -1,3 +1,5 @@
+use std::iter::zip;
+
 use crate::tensor::Tensor;
 
 /// Check that the shapes of two tensors are equal and that their contents
@@ -11,18 +13,22 @@ pub fn expect_equal(x: &Tensor, y: &Tensor) -> Result<(), String> {
         ));
     }
 
+    let mut mismatches = 0;
     let eps = 0.001;
-    for i in 0..x.len() {
-        let xi = x.data()[i];
-        let yi = y.data()[i];
 
+    for (xi, yi) in zip(x.elements(), y.elements()) {
         if (xi - yi).abs() > eps {
-            return Err(format!(
-                "Tensor values differ at index {}: {} vs {}",
-                i, xi, yi
-            ));
+            mismatches += 1;
         }
     }
 
-    Ok(())
+    if mismatches > 0 {
+        Err(format!(
+            "Tensor values differ at {} of {} indexes",
+            mismatches,
+            x.len()
+        ))
+    } else {
+        Ok(())
+    }
 }
