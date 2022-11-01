2ddafd1f43e60a99994cc93bb938989d37c3ec1e
Robert Knight <robertknight@gmail.com>
2022-11-01 06:00:34 +0000

    Extract activation operators into separate module

---
+++ src/ops/activations.rs
@@ -0,0 +1,220 @@
+use crate::ops::{Input, Operator, Output};
+use crate::tensor::Tensor;
+
+pub fn clip(input: &Tensor, min: f32, max: f32) -> Tensor {
+    input.map(|x| x.max(min).min(max))
+}
+
+pub fn clip_in_place(input: &mut Tensor, min: f32, max: f32) {
+    for val in input.data_mut().iter_mut() {
+        *val = val.max(min).min(max)
+    }
+}
+
+#[derive(Debug)]
+pub struct Clip {
+    pub min: f32,
+    pub max: f32,
+}
+
+impl Operator for Clip {
+    fn name(&self) -> &str {
+        "Clip"
+    }
+
+    fn run(&self, inputs: &[Input]) -> Output {
+        let input = inputs[0].as_float().unwrap();
+        clip(input, self.min, self.max).into()
+    }
+
+    fn can_run_in_place(&self) -> bool {
+        true
+    }
+
+    fn run_in_place(&self, input: Output, _: &[Input]) -> Output {
+        let mut output = input.as_float().unwrap();
+        clip_in_place(&mut output, self.min, self.max);
+        output.into()
+    }
+}
+
+pub fn leaky_relu(input: &Tensor, alpha: f32) -> Tensor {
+    input.map(|el| if el < 0.0 { alpha * el } else { el })
+}
+
+pub fn leaky_relu_in_place(input: &mut Tensor, alpha: f32) {
+    for val in input.data_mut().iter_mut() {
+        *val = if (*val) < 0.0 { alpha * (*val) } else { *val }
+    }
+}
+
+#[derive(Debug)]
+pub struct LeakyRelu {
+    pub alpha: f32,
+}
+
+impl Operator for LeakyRelu {
+    fn name(&self) -> &str {
+        "LeakyRelu"
+    }
+
+    fn run(&self, inputs: &[Input]) -> Output {
+        let input = inputs[0].as_float().unwrap();
+        leaky_relu(input, self.alpha).into()
+    }
+
+    fn can_run_in_place(&self) -> bool {
+        true
+    }
+
+    fn run_in_place(&self, input: Output, _other: &[Input]) -> Output {
+        let mut output = input.as_float().unwrap();
+        leaky_relu_in_place(&mut output, self.alpha);
+        output.into()
+    }
+}
+
+pub fn relu_in_place(x: &mut Tensor) {
+    for val in x.data_mut().iter_mut() {
+        *val = val.max(0f32);
+    }
+}
+
+pub fn relu(x: &Tensor) -> Tensor {
+    x.map(|e| e.max(0f32))
+}
+
+#[derive(Debug)]
+pub struct ReLU {}
+impl Operator for ReLU {
+    fn name(&self) -> &str {
+        "ReLU"
+    }
+
+    /// Run `relu` operator with `[input]` inputs.
+    fn run(&self, inputs: &[Input]) -> Output {
+        let input = inputs[0].as_float().unwrap();
+        relu(input).into()
+    }
+
+    fn can_run_in_place(&self) -> bool {
+        true
+    }
+
+    fn run_in_place(&self, input: Output, _other: &[Input]) -> Output {
+        let mut output = input.as_float().unwrap();
+        relu_in_place(&mut output);
+        output.into()
+    }
+}
+
+pub fn sigmoid(x: &Tensor) -> Tensor {
+    x.map(|e| 1. / (1. + (-e).exp()))
+}
+
+pub fn sigmoid_in_place(x: &mut Tensor) {
+    for val in x.data_mut().iter_mut() {
+        *val = 1. / (1. + (-*val).exp());
+    }
+}
+
+#[derive(Debug)]
+pub struct Sigmoid {}
+impl Operator for Sigmoid {
+    fn name(&self) -> &str {
+        "Sigmoid"
+    }
+
+    fn run(&self, inputs: &[Input]) -> Output {
+        let input = inputs[0].as_float().unwrap();
+        sigmoid(input).into()
+    }
+
+    fn can_run_in_place(&self) -> bool {
+        true
+    }
+
+    fn run_in_place(&self, input: Output, _other: &[Input]) -> Output {
+        let mut output = input.as_float().unwrap();
+        sigmoid_in_place(&mut output);
+        output.into()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ops::{
+        clip, clip_in_place, leaky_relu, leaky_relu_in_place, relu, relu_in_place, sigmoid,
+        sigmoid_in_place,
+    };
+    use crate::tensor::from_data;
+    use crate::test_util::expect_equal;
+
+    #[test]
+    fn test_clip() -> Result<(), String> {
+        let input = from_data(vec![2, 2], vec![-5., -2., 3., 20.]);
+        let expected = from_data(vec![2, 2], vec![1., 1., 3., 5.]);
+        let result = clip(&input, 1.0, 5.0);
+        expect_equal(&result, &expected)
+    }
+
+    #[test]
+    fn test_clip_in_place() -> Result<(), String> {
+        let mut input = from_data(vec![2, 2], vec![-5., -2., 3., 20.]);
+        let expected = from_data(vec![2, 2], vec![1., 1., 3., 5.]);
+        clip_in_place(&mut input, 1.0, 5.0);
+        expect_equal(&input, &expected)
+    }
+
+    #[test]
+    fn test_leaky_relu() -> Result<(), String> {
+        let input = from_data(vec![2, 2], vec![-5., -2., 3., 20.]);
+        let alpha = 0.1;
+        let expected = from_data(vec![2, 2], vec![-5. * alpha, -2. * alpha, 3., 20.]);
+        let result = leaky_relu(&input, alpha);
+        expect_equal(&result, &expected)
+    }
+
+    #[test]
+    fn test_leaky_relu_in_place() -> Result<(), String> {
+        let mut input = from_data(vec![2, 2], vec![-5., -2., 3., 20.]);
+        let alpha = 0.1;
+        let expected = from_data(vec![2, 2], vec![-5. * alpha, -2. * alpha, 3., 20.]);
+        leaky_relu_in_place(&mut input, alpha);
+        expect_equal(&input, &expected)
+    }
+
+    #[test]
+    fn test_relu() -> Result<(), String> {
+        let input = from_data(vec![2, 2, 1], vec![-0.5, 0.5, 3.0, -5.5]);
+        let expected = from_data(vec![2, 2, 1], vec![0.0, 0.5, 3.0, 0.0]);
+
+        let result = relu(&input);
+        expect_equal(&result, &expected)?;
+
+        let mut result = input.clone();
+        relu_in_place(&mut result);
+        expect_equal(&result, &expected)
+    }
+
+    #[test]
+    fn test_sigmoid() -> Result<(), String> {
+        let input = from_data(
+            vec![9],
+            vec![-500.0, -3.0, -1.0, -0.5, 0.0, 0.5, 1.0, 3.0, 500.0],
+        );
+        let expected = from_data(
+            vec![9],
+            vec![
+                0.0000, 0.0474, 0.2689, 0.3775, 0.5000, 0.6225, 0.7311, 0.9526, 1.0000,
+            ],
+        );
+
+        let result = sigmoid(&input);
+        expect_equal(&result, &expected)?;
+
+        let mut result = input.clone();
+        sigmoid_in_place(&mut result);
+        expect_equal(&result, &expected)
+    }
+}
--- src/ops/mod.rs
+++ src/ops/mod.rs
@@ -4,8 +4,14 @@ use std::iter::zip;
 use crate::linalg::{gemm, gemm_slice, Matrix};
 use crate::tensor::{from_data, from_scalar, zero_tensor, Tensor};
 
+mod activations;
 mod conv;
 
+pub use activations::{
+    clip, clip_in_place, leaky_relu, leaky_relu_in_place, relu, relu_in_place, sigmoid,
+    sigmoid_in_place,
+};
+pub use activations::{Clip, LeakyRelu, ReLU, Sigmoid};
 pub use conv::{conv_2d, conv_transpose_2d};
 pub use conv::{Conv2d, ConvTranspose2d, Padding};
 
@@ -245,16 +251,6 @@ impl Operator for Add {
     }
 }
 
-pub fn clip(input: &Tensor, min: f32, max: f32) -> Tensor {
-    input.map(|x| x.max(min).min(max))
-}
-
-pub fn clip_in_place(input: &mut Tensor, min: f32, max: f32) {
-    for val in input.data_mut().iter_mut() {
-        *val = val.max(min).min(max)
-    }
-}
-
 /// Perform in-place batch normalization on the NCHW tensor `out`.
 ///
 /// See https://github.com/onnx/onnx/blob/main/docs/Operators.md#batchnormalization
@@ -332,33 +328,6 @@ impl Operator for BatchNormalization {
 
         batch_norm_in_place(&mut output, scale, bias, mean, var, self.epsilon);
 
-        output.into()
-    }
-}
-
-#[derive(Debug)]
-pub struct Clip {
-    pub min: f32,
-    pub max: f32,
-}
-
-impl Operator for Clip {
-    fn name(&self) -> &str {
-        "Clip"
-    }
-
-    fn run(&self, inputs: &[Input]) -> Output {
-        let input = inputs[0].as_float().unwrap();
-        clip(input, self.min, self.max).into()
-    }
-
-    fn can_run_in_place(&self) -> bool {
-        true
-    }
-
-    fn run_in_place(&self, input: Output, _: &[Input]) -> Output {
-        let mut output = input.as_float().unwrap();
-        clip_in_place(&mut output, self.min, self.max);
         output.into()
     }
 }
@@ -528,42 +497,6 @@ impl Operator for GlobalAveragePool {
     }
 }
 
-pub fn leaky_relu(input: &Tensor, alpha: f32) -> Tensor {
-    input.map(|el| if el < 0.0 { alpha * el } else { el })
-}
-
-pub fn leaky_relu_in_place(input: &mut Tensor, alpha: f32) {
-    for val in input.data_mut().iter_mut() {
-        *val = if (*val) < 0.0 { alpha * (*val) } else { *val }
-    }
-}
-
-#[derive(Debug)]
-pub struct LeakyRelu {
-    pub alpha: f32,
-}
-
-impl Operator for LeakyRelu {
-    fn name(&self) -> &str {
-        "LeakyRelu"
-    }
-
-    fn run(&self, inputs: &[Input]) -> Output {
-        let input = inputs[0].as_float().unwrap();
-        leaky_relu(input, self.alpha).into()
-    }
-
-    fn can_run_in_place(&self) -> bool {
-        true
-    }
-
-    fn run_in_place(&self, input: Output, _other: &[Input]) -> Output {
-        let mut output = input.as_float().unwrap();
-        leaky_relu_in_place(&mut output, self.alpha);
-        output.into()
-    }
-}
-
 pub fn max_pool_2d(input: &Tensor, kernel_size: usize) -> Tensor {
     let [batch, in_c, in_h, in_w] = input.dims();
     let out_h = in_h / kernel_size;
@@ -681,40 +614,6 @@ impl Operator for Mul {
     }
 }
 
-pub fn relu_in_place(x: &mut Tensor) {
-    for val in x.data_mut().iter_mut() {
-        *val = val.max(0f32);
-    }
-}
-
-pub fn relu(x: &Tensor) -> Tensor {
-    x.map(|e| e.max(0f32))
-}
-
-#[derive(Debug)]
-pub struct ReLU {}
-impl Operator for ReLU {
-    fn name(&self) -> &str {
-        "ReLU"
-    }
-
-    /// Run `relu` operator with `[input]` inputs.
-    fn run(&self, inputs: &[Input]) -> Output {
-        let input = inputs[0].as_float().unwrap();
-        relu(input).into()
-    }
-
-    fn can_run_in_place(&self) -> bool {
-        true
-    }
-
-    fn run_in_place(&self, input: Output, _other: &[Input]) -> Output {
-        let mut output = input.as_float().unwrap();
-        relu_in_place(&mut output);
-        output.into()
-    }
-}
-
 pub fn reshape<T: Copy>(input: &Tensor<T>, shape: &Tensor<i32>) -> Tensor<T> {
     // If exactly one of the new shape's dimensions is -1, infer the size
     // from the input length and the sizes of the other dimensions.
@@ -789,39 +688,6 @@ impl Operator for Shape {
             input.shape().iter().map(|&el| el as i32).collect(),
         );
         shape.into()
-    }
-}
-
-pub fn sigmoid(x: &Tensor) -> Tensor {
-    x.map(|e| 1. / (1. + (-e).exp()))
-}
-
-pub fn sigmoid_in_place(x: &mut Tensor) {
-    for val in x.data_mut().iter_mut() {
-        *val = 1. / (1. + (-*val).exp());
-    }
-}
-
-#[derive(Debug)]
-pub struct Sigmoid {}
-impl Operator for Sigmoid {
-    fn name(&self) -> &str {
-        "Sigmoid"
-    }
-
-    fn run(&self, inputs: &[Input]) -> Output {
-        let input = inputs[0].as_float().unwrap();
-        sigmoid(input).into()
-    }
-
-    fn can_run_in_place(&self) -> bool {
-        true
-    }
-
-    fn run_in_place(&self, input: Output, _other: &[Input]) -> Output {
-        let mut output = input.as_float().unwrap();
-        sigmoid_in_place(&mut output);
-        output.into()
     }
 }
 
@@ -1053,9 +919,8 @@ impl Operator for Unsqueeze {
 mod tests {
     use crate::linalg::gemm;
     use crate::ops::{
-        add, add_in_place, batch_norm, batch_norm_in_place, clip, clip_in_place, concat, gather,
-        gemm_op, global_average_pool, leaky_relu, leaky_relu_in_place, matmul, max_pool_2d, mul,
-        mul_in_place, pad_2d, relu, relu_in_place, reshape, sigmoid, sigmoid_in_place, slice,
+        add, add_in_place, batch_norm, batch_norm_in_place, concat, gather, gemm_op,
+        global_average_pool, matmul, max_pool_2d, mul, mul_in_place, pad_2d, reshape, slice,
         slice_in_place, unsqueeze, Add, Operator, Output, Reshape, Shape,
     };
     use crate::rng::XorShiftRNG;
@@ -1167,22 +1032,6 @@ mod tests {
     }
 
     #[test]
-    fn test_clip() -> Result<(), String> {
-        let input = from_data(vec![2, 2], vec![-5., -2., 3., 20.]);
-        let expected = from_data(vec![2, 2], vec![1., 1., 3., 5.]);
-        let result = clip(&input, 1.0, 5.0);
-        expect_equal(&result, &expected)
-    }
-
-    #[test]
-    fn test_clip_in_place() -> Result<(), String> {
-        let mut input = from_data(vec![2, 2], vec![-5., -2., 3., 20.]);
-        let expected = from_data(vec![2, 2], vec![1., 1., 3., 5.]);
-        clip_in_place(&mut input, 1.0, 5.0);
-        expect_equal(&input, &expected)
-    }
-
-    #[test]
     fn test_gather() {
         // We currently support only one common use of Gather, which is to
         // index into a vector with a scalar, eg. to extract one dimension from
@@ -1249,24 +1098,6 @@ mod tests {
     }
 
     #[test]
-    fn test_leaky_relu() -> Result<(), String> {
-        let input = from_data(vec![2, 2], vec![-5., -2., 3., 20.]);
-        let alpha = 0.1;
-        let expected = from_data(vec![2, 2], vec![-5. * alpha, -2. * alpha, 3., 20.]);
-        let result = leaky_relu(&input, alpha);
-        expect_equal(&result, &expected)
-    }
-
-    #[test]
-    fn test_leaky_relu_in_place() -> Result<(), String> {
-        let mut input = from_data(vec![2, 2], vec![-5., -2., 3., 20.]);
-        let alpha = 0.1;
-        let expected = from_data(vec![2, 2], vec![-5. * alpha, -2. * alpha, 3., 20.]);
-        leaky_relu_in_place(&mut input, alpha);
-        expect_equal(&input, &expected)
-    }
-
-    #[test]
     fn test_matmul() -> Result<(), String> {
         let mut rng = XorShiftRNG::new(1234);
         let a = random_tensor(&[3, 10], &mut rng);
@@ -1322,19 +1153,6 @@ mod tests {
     }
 
     #[test]
-    fn test_relu() -> Result<(), String> {
-        let input = from_data(vec![2, 2, 1], vec![-0.5, 0.5, 3.0, -5.5]);
-        let expected = from_data(vec![2, 2, 1], vec![0.0, 0.5, 3.0, 0.0]);
-
-        let result = relu(&input);
-        expect_equal(&result, &expected)?;
-
-        let mut result = input.clone();
-        relu_in_place(&mut result);
-        expect_equal(&result, &expected)
-    }
-
-    #[test]
     fn test_reshape_with_unspecified_dim() -> Result<(), String> {
         // Reshape with an unspecified (-1) dim and nonzero-length input
         let input = from_data(vec![2, 2], vec![-0.5, 0.5, 3.0, -5.5]);
@@ -1379,27 +1197,6 @@ mod tests {
             .as_float()
             .unwrap();
 
-        expect_equal(&result, &expected)
-    }
-
-    #[test]
-    fn test_sigmoid() -> Result<(), String> {
-        let input = from_data(
-            vec![9],
-            vec![-500.0, -3.0, -1.0, -0.5, 0.0, 0.5, 1.0, 3.0, 500.0],
-        );
-        let expected = from_data(
-            vec![9],
-            vec![
-                0.0000, 0.0474, 0.2689, 0.3775, 0.5000, 0.6225, 0.7311, 0.9526, 1.0000,
-            ],
-        );
-
-        let result = sigmoid(&input);
-        expect_equal(&result, &expected)?;
-
-        let mut result = input.clone();
-        sigmoid_in_place(&mut result);
         expect_equal(&result, &expected)
     }
 
