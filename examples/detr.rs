516eb5576a18461a95decfa4bc619c5181b6555a
Robert Knight <robertknight@gmail.com>
2023-10-20 09:12:06 +0100

    Extract common image IO functions into a helper crate
    There are now several examples with similar code for reading/writing images
    from/to CHW tensors and applying ImageNet normalization.

--- examples/detr.rs
+++ examples/detr.rs
@@ -1,16 +1,15 @@
 use std::collections::VecDeque;
 use std::error::Error;
 use std::fs;
-use std::io::BufWriter;
-use std::iter::zip;
 
 use wasnn::ops::{
     arg_max, resize, softmax, CoordTransformMode, NearestMode, ResizeMode, ResizeTarget,
 };
 use wasnn::{Model, NodeId, RunOptions};
+use wasnn_imageio::{normalize_image, read_image, write_image};
 use wasnn_imageproc::{Painter, Rect};
 use wasnn_tensor::prelude::*;
-use wasnn_tensor::{ndtensor, NdTensor, NdTensorView, NdTensorViewMut};
+use wasnn_tensor::{ndtensor, NdTensor, NdTensorView};
 
 struct Args {
     model: String,
@@ -61,22 +60,6 @@ Options:
     };
 
     Ok(args)
-}
-
-/// Apply standard ImageNet normalization to a pixel value.
-/// See https://huggingface.co/facebook/detr-resnet-50#preprocessing.
-fn normalize_pixel(value: f32, channel: usize) -> f32 {
-    assert!(channel < 3, "channel index is invalid");
-    let imagenet_mean = [0.485, 0.456, 0.406];
-    let imagenet_std_dev = [0.229, 0.224, 0.225];
-    (value - imagenet_mean[channel]) / imagenet_std_dev[channel]
-}
-
-/// Apply standard ImageNet normalization to all pixel values in an image.
-fn normalize_image(mut img: NdTensorViewMut<f32, 3>) {
-    for ([chan, _y, _x], pixel) in zip(img.indices(), img.iter_mut()) {
-        *pixel = normalize_pixel(*pixel, chan);
-    }
 }
 
 /// Calculate rescaled size for an image which currently has dimensions `(width,
@@ -130,60 +113,6 @@ fn rescaled_size(
     } else {
         (new_long.floor() as usize, new_short.ceil() as usize)
     }
-}
-
-/// Read an image from `path` into a CHW tensor.
-fn read_image(path: &str) -> Result<NdTensor<f32, 3>, Box<dyn Error>> {
-    let input_img = image::open(path)?;
-    let input_img = input_img.into_rgb8();
-
-    let (width, height) = input_img.dimensions();
-
-    let in_chans = 3;
-    let mut float_img = NdTensor::zeros([in_chans, height as usize, width as usize]);
-    for c in 0..in_chans {
-        let mut chan_img = float_img.slice_mut([c]);
-        for y in 0..height {
-            for x in 0..width {
-                let pixel_value = input_img.get_pixel(x, y)[c] as f32 / 255.0;
-                chan_img[[y as usize, x as usize]] = pixel_value;
-            }
-        }
-    }
-    Ok(float_img)
-}
-
-/// Convert a CHW float tensor with values in the range [0, 1] to `Vec<u8>` with
-/// values scaled to [0, 255].
-fn image_from_tensor(tensor: NdTensorView<f32, 3>) -> Vec<u8> {
-    tensor
-        .iter()
-        .map(|x| (x.clamp(0., 1.) * 255.0) as u8)
-        .collect()
-}
-
-/// Write a CHW image to a PNG file in `path`.
-fn write_image(path: &str, img: NdTensorView<f32, 3>) -> Result<(), Box<dyn Error>> {
-    let img_width = img.size(2);
-    let img_height = img.size(1);
-    let color_type = match img.size(0) {
-        1 => png::ColorType::Grayscale,
-        3 => png::ColorType::Rgb,
-        4 => png::ColorType::Rgba,
-        _ => return Err("Unsupported channel count".into()),
-    };
-
-    let hwc_img = img.permuted([1, 2, 0]); // CHW => HWC
-
-    let out_img = image_from_tensor(hwc_img);
-    let file = fs::File::create(path)?;
-    let writer = BufWriter::new(file);
-    let mut encoder = png::Encoder::new(writer, img_width as u32, img_height as u32);
-    encoder.set_color(color_type);
-    let mut writer = encoder.write_header()?;
-    writer.write_image_data(&out_img)?;
-
-    Ok(())
 }
 
 fn get_node(model: &Model, name: &str) -> Result<NodeId, String> {
