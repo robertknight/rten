22e043abd5d59816e4bf4564f0530784ff6f04f7
Robert Knight <robertknight@gmail.com>
2023-10-29 17:22:58 +0000

    WIP - Use torchtext T5 model in T5 example

--- examples/t5.rs
+++ examples/t5.rs
@@ -7,8 +7,7 @@ use wasnn_tensor::prelude::*;
 use wasnn_tensor::{tensor, NdTensor, Tensor};
 
 struct Args {
-    encoder_model: String,
-    decoder_model: String,
+    model: String,
 }
 
 fn parse_args() -> Result<Args, lexopt::Error> {
@@ -24,12 +23,11 @@ fn parse_args() -> Result<Args, lexopt::Error> {
                 println!(
                     "Perform semantic segmentation on an image.
 
-Usage: {bin_name} <encoder_model> <decoder_model>
+Usage: {bin_name} <model>
 
 Args:
 
-  <encoder_model> - T5 encoder model
-  <decoder_model> - T5 decoder model
+  <model> - T5 combined encoder + decoder model
 ",
                     bin_name = parser.bin_name().unwrap_or("t5")
                 );
@@ -39,13 +37,9 @@ Args:
         }
     }
 
-    let encoder_model = values.pop_front().ok_or("missing `encoder_model` arg")?;
-    let decoder_model = values.pop_front().ok_or("missing `decoder_model` arg")?;
+    let model = values.pop_front().ok_or("missing `model` arg")?;
 
-    let args = Args {
-        encoder_model,
-        decoder_model,
-    };
+    let args = Args { model };
 
     Ok(args)
 }
@@ -58,53 +52,41 @@ fn find_node(model: &Model, name: &str) -> Result<NodeId, String> {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = parse_args()?;
-    let encoder_model_bytes = fs::read(args.encoder_model)?;
-    let encoder_model = Model::load(&encoder_model_bytes)?;
-    let decoder_model_bytes = fs::read(args.decoder_model)?;
-    let decoder_model = Model::load(&decoder_model_bytes)?;
+    let model_bytes = fs::read(args.model)?;
+    let model = Model::load(&model_bytes)?;
 
-    let input_ids_id = find_node(&encoder_model, "input_ids")?;
-    let attention_mask_id = find_node(&encoder_model, "attention_mask")?;
-    let output_id = find_node(&encoder_model, "last_hidden_state")?;
+    // "translate to french: studies have shown that owning a dog is good for you"
+    let mut encoder_tokens = tensor!([
+        13959, 12, 20609, 10, 2116, 43, 2008, 24, 293, 53, 3, 9, 1782, 19, 207, 21, 25, 1
+    ]);
+    encoder_tokens.insert_dim(0);
 
-    // "summarize: studies have shown that owning a dog is good for you"
-    let mut input_ids =
-        tensor!([21603, 10, 2116, 43, 2008, 24, 293, 53, 3, 9, 1782, 19, 207, 21, 25, 1]);
-    input_ids.insert_dim(0);
-    let attention_mask = Tensor::full(input_ids.shape(), 1i32);
-
-    let [encoder_state] = encoder_model.run_n(
-        &[
-            (input_ids_id, (&input_ids).into()),
-            (attention_mask_id, (&attention_mask).into()),
-        ],
-        [output_id],
-        Some(RunOptions {
-            timing: false,
-            verbose: false,
-        }),
-    )?;
-
-    let encoder_state: Tensor<f32> = encoder_state.try_into()?;
+    // expected output:
+    //
+    // "Des études ont montré que la propriété d’un chien est bonne pour vous"
+    //
+    // [
+    //     0, 2973, 17868, 30, 17, 29625, 238, 50, 19713, 3, 26, 22, 202, 17826, 259, 4079, 171, 327,
+    //     1,
+    // ];
 
     let mut output_tokens: Vec<i32> = vec![0];
 
     let eos_token = 1;
     while output_tokens.last() != Some(&eos_token) {
-        let mut decoder_input_ids: Tensor<i32> = Tensor::from_vec(output_tokens.clone());
-        decoder_input_ids.insert_dim(0);
-        let decoder_input_ids_id = find_node(&decoder_model, "input_ids")?;
-        let encoder_attention_mask_id = find_node(&decoder_model, "encoder_attention_mask")?;
-        let encoder_hidden_states_id = find_node(&decoder_model, "encoder_hidden_states")?;
-        let decoder_logits_id = find_node(&decoder_model, "logits")?;
+        let mut decoder_tokens: Tensor<i32> = Tensor::from_vec(output_tokens.clone());
+        decoder_tokens.insert_dim(0);
 
-        let [logits] = decoder_model.run_n(
+        let encoder_tokens_id = find_node(&model, "encoder_tokens")?;
+        let decoder_tokens_id = find_node(&model, "decoder_tokens")?;
+        let logits_id = find_node(&model, "logits")?;
+
+        let [logits] = model.run_n(
             &[
-                (decoder_input_ids_id, (&decoder_input_ids).into()),
-                (encoder_attention_mask_id, (&attention_mask).into()),
-                (encoder_hidden_states_id, (&encoder_state).into()),
+                (encoder_tokens_id, (&encoder_tokens).into()),
+                (decoder_tokens_id, (&decoder_tokens).into()),
             ],
-            [decoder_logits_id],
+            [logits_id],
             Some(RunOptions {
                 timing: false,
                 verbose: false,
