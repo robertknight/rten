432a2a9bff98b59239d1df372e4220124e0d4f5d
Robert Knight <robertknight@gmail.com>
2023-12-24 20:47:16 +0000

    Add `range_chunks_exact` utility
    This is not currently used, but I have an upcoming use for it in matrix
    prepacking.

--- src/iter_util.rs
+++ src/iter_util.rs
@@ -2,9 +2,7 @@ use std::ops::Range;
 
 /// Iterator returned by [range_chunks].
 pub struct RangeChunks {
-    /// Remaining range.
-    range: Range<usize>,
-
+    remainder: Range<usize>,
     chunk_size: usize,
 }
 
@@ -12,10 +10,10 @@ impl Iterator for RangeChunks {
     type Item = Range<usize>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if !self.range.is_empty() {
-            let start = self.range.start;
-            let end = (start + self.chunk_size).min(self.range.end);
-            self.range.start += self.chunk_size;
+        if !self.remainder.is_empty() {
+            let start = self.remainder.start;
+            let end = (start + self.chunk_size).min(self.remainder.end);
+            self.remainder.start += self.chunk_size;
             Some(start..end)
         } else {
             None
@@ -23,7 +21,7 @@ impl Iterator for RangeChunks {
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
-        let len = self.range.len().div_ceil(self.chunk_size);
+        let len = self.remainder.len().div_ceil(self.chunk_size);
         (len, Some(len))
     }
 }
@@ -35,12 +33,65 @@ impl std::iter::FusedIterator for RangeChunks {}
 /// Return an iterator over sub-ranges of `range`. If `range.len()` is not a
 /// multiple of `chunk_size` then the final chunk will be shorter.
 pub fn range_chunks(range: Range<usize>, chunk_size: usize) -> RangeChunks {
-    RangeChunks { range, chunk_size }
+    RangeChunks {
+        remainder: range,
+        chunk_size,
+    }
+}
+
+pub struct RangeChunksExact {
+    remainder: Range<usize>,
+    chunk_size: usize,
+}
+
+impl RangeChunksExact {
+    /// Return the part of the range that has not yet been visited.
+    #[allow(dead_code)]
+    pub fn remainder(&self) -> Range<usize> {
+        self.remainder.clone()
+    }
+}
+
+impl Iterator for RangeChunksExact {
+    type Item = Range<usize>;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.remainder.len() >= self.chunk_size {
+            let start = self.remainder.start;
+            let end = start + self.chunk_size;
+            self.remainder.start += self.chunk_size;
+            Some(start..end)
+        } else {
+            None
+        }
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len = self.remainder.len() / self.chunk_size;
+        (len, Some(len))
+    }
+}
+
+impl ExactSizeIterator for RangeChunksExact {}
+
+impl std::iter::FusedIterator for RangeChunksExact {}
+
+/// Return an iterator over sub-ranges of `range`. If `range.len()` is not a
+/// multiple of `chunk_size` then there will be a remainder after iteration
+/// completes, available via [RangeChunksExact::remainder].
+#[allow(dead_code)]
+pub fn range_chunks_exact(range: Range<usize>, chunk_size: usize) -> RangeChunksExact {
+    RangeChunksExact {
+        remainder: range,
+        chunk_size,
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::range_chunks;
+    use super::{range_chunks, range_chunks_exact};
 
     #[test]
     fn test_range_chunks() {
@@ -61,5 +112,27 @@ mod tests {
         assert_eq!(chunks.next(), Some(10..13));
         assert_eq!(chunks.next(), None);
         assert_eq!(chunks.next(), None);
+    }
+
+    #[test]
+    fn test_range_chunks_exact() {
+        // All chunks full (empty remainder).
+        let mut chunks = range_chunks_exact(0..15, 5);
+        assert_eq!(chunks.size_hint(), (3, Some(3)));
+        assert_eq!(chunks.next(), Some(0..5));
+        assert_eq!(chunks.next(), Some(5..10));
+        assert_eq!(chunks.next(), Some(10..15));
+        assert_eq!(chunks.next(), None);
+        assert_eq!(chunks.next(), None);
+        assert_eq!(chunks.remainder(), 15..15);
+
+        // Non-empty remainder
+        let mut chunks = range_chunks_exact(0..13, 5);
+        assert_eq!(chunks.size_hint(), (2, Some(2)));
+        assert_eq!(chunks.next(), Some(0..5));
+        assert_eq!(chunks.next(), Some(5..10));
+        assert_eq!(chunks.next(), None);
+        assert_eq!(chunks.next(), None);
+        assert_eq!(chunks.remainder(), 10..13);
     }
 }
