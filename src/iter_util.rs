703ba548080423996427a598964ef564137054ba
Robert Knight <robertknight@gmail.com>
2023-12-28 10:39:18 +0000

    Add utility for optional parallelism
    This will allow for disabling Rayon at runtime to reduce overhead in a
    single-threaded environment or to make debugging easier.
    
    The wrapped iterator only supports the `.for_each` consumer, because that's all
    I need for the moment.

--- src/iter_util.rs
+++ src/iter_util.rs
@@ -1,5 +1,7 @@
 use std::ops::Range;
 
+use rayon::prelude::*;
+
 /// Iterator returned by [range_chunks].
 pub struct RangeChunks {
     remainder: Range<usize>,
@@ -89,9 +91,54 @@ pub fn range_chunks_exact(range: Range<usize>, chunk_size: usize) -> RangeChunks
     }
 }
 
+/// Wrapper around either a serial or parallel iterator, returned by
+/// [MaybeParIter::maybe_par_iter].
+pub enum MaybeParallel<PI: ParallelIterator, SI: Iterator<Item = PI::Item>> {
+    Serial(SI),
+    Parallel(PI),
+}
+
+impl<PI: ParallelIterator, SI: Iterator<Item = PI::Item>> MaybeParallel<PI, SI> {
+    pub fn for_each<F: Fn(PI::Item) + Send + Sync>(self, f: F) {
+        match self {
+            MaybeParallel::Serial(iter) => iter.for_each(f),
+            MaybeParallel::Parallel(iter) => iter.for_each(f),
+        }
+    }
+}
+
+/// Trait which allows use of Rayon parallelism to be conditionally enabled.
+///
+/// See https://crates.io/crates/rayon-cond for a more full-featured alternative.
+pub trait MaybeParIter {
+    type Item;
+    type ParIter: ParallelIterator<Item = Self::Item>;
+    type Iter: Iterator<Item = Self::Item>;
+
+    /// Return an iterator which executes either in serial on the current
+    /// thread, or in parallel in a Rayon thread pool if `parallel` is true.
+    fn maybe_par_iter(self, parallel: bool) -> MaybeParallel<Self::ParIter, Self::Iter>;
+}
+
+impl MaybeParIter for Range<usize> {
+    type Item = usize;
+    type ParIter = rayon::range::Iter<usize>;
+    type Iter = Range<usize>;
+
+    fn maybe_par_iter(self, parallel: bool) -> MaybeParallel<Self::ParIter, Self::Iter> {
+        if parallel {
+            MaybeParallel::Parallel(self.into_par_iter())
+        } else {
+            MaybeParallel::Serial(self)
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{range_chunks, range_chunks_exact};
+    use std::sync::atomic::{AtomicU32, Ordering};
+
+    use super::{range_chunks, range_chunks_exact, MaybeParIter};
 
     #[test]
     fn test_range_chunks() {
@@ -134,5 +181,20 @@ mod tests {
         assert_eq!(chunks.next(), None);
         assert_eq!(chunks.next(), None);
         assert_eq!(chunks.remainder(), 10..13);
+    }
+
+    #[test]
+    fn test_maybe_par_iter() {
+        let count = AtomicU32::new(0);
+        (0..1000).maybe_par_iter(false).for_each(|_| {
+            count.fetch_add(1, Ordering::SeqCst);
+        });
+        assert_eq!(count.load(Ordering::SeqCst), 1000);
+
+        let count = AtomicU32::new(0);
+        (0..1000).maybe_par_iter(true).for_each(|_| {
+            count.fetch_add(1, Ordering::SeqCst);
+        });
+        assert_eq!(count.load(Ordering::SeqCst), 1000);
     }
 }
