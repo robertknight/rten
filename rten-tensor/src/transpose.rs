da1a02d45f10b20ba4896177baa8d8f6fcafde87
Robert Knight <robertknight@gmail.com>
2024-03-17 06:50:24 +0000

    Optimize transpose operations by reading source in contiguous order
    Previously `Tensor::to_tensor` would read the source in logical (non-contiguous)
    order and write to the destination in contiguous order.  When the source is not
    in cache, it is faster to do the opposite.
    
    From `bench_transpose`, the `[1, 1500, 8, 64] => [1, 8, 64, 1500]` case goes
    from ~1.5ms average to ~0.6ms, compared to ~0.2ms for a simple copy.

--- rten-tensor/src/transpose.rs
+++ rten-tensor/src/transpose.rs
@@ -1,65 +1,74 @@
 use crate::{AsView, Layout};
-use crate::{NdTensorView, TensorView};
-
-/// Call `f` with every element in `x` in logical order.
-///
-/// This is equivalent to `x.iter().for_each(f)` but is faster that Rust's
-/// standard iteration protocol when `x` is non-contiguous and has <= 4
-/// dimensions.
-fn fast_for_each_element<T, F: FnMut(&T)>(mut x: TensorView<T>, mut f: F) {
-    if x.ndim() > 4 {
-        x.iter().for_each(f)
-    } else {
-        while x.ndim() < 4 {
-            x.insert_axis(0);
-        }
-
-        let x_data = x.non_contiguous_data();
-        let x: NdTensorView<T, 4> = x.nd_view();
-        let shape = x.shape();
-        let strides = x.strides();
-
-        assert!(x_data.len() >= x.layout().min_data_len());
-
-        for i0 in 0..shape[0] {
-            for i1 in 0..shape[1] {
-                for i2 in 0..shape[2] {
-                    for i3 in 0..shape[3] {
-                        let offset =
-                            i0 * strides[0] + i1 * strides[1] + i2 * strides[2] + i3 * strides[3];
-
-                        // Safety: We checked data length > max offset produced
-                        // by layout.
-                        let elt = unsafe { x_data.get_unchecked(offset) };
-                        f(elt)
-                    }
-                }
-            }
-        }
-    }
-}
+use crate::{NdLayout, NdTensorView, TensorView};
 
 /// Return the elements of `src` as a contiguous vector, in the same order they
 /// would be yielded by `src.iter()`.
 ///
 /// This is equivalent to `src.iter().cloned().collect::<Vec<_>>()` but
 /// faster.
-pub fn contiguous_data<T: Clone>(src: TensorView<T>) -> Vec<T> {
+pub fn contiguous_data<T: Clone>(mut src: TensorView<T>) -> Vec<T> {
+    if src.ndim() > 4 {
+        // Fallback for tensors with too many dims.
+        return src.iter().cloned().collect();
+    }
+
     let src_len = src.len();
 
-    // This is equivalent to `x.iter().cloned().collect::<Vec<_>>()` but uses a
-    // faster iteration method that is optimized for tensors with few (<= 4)
-    // dimensions.
     let mut data = Vec::with_capacity(src.len());
-    let ptr: *mut T = data.as_mut_ptr();
 
-    let mut offset = 0;
-    fast_for_each_element(src, |elt| {
-        // Safety: `fast_for_each_element` calls fn `self.len()` times,
-        // matching the buffer capacity.
-        unsafe { *ptr.add(offset) = elt.clone() };
-        offset += 1;
+    while src.ndim() < 4 {
+        src.insert_axis(0);
+    }
+
+    let src_data = src.non_contiguous_data();
+
+    let src_4d: NdTensorView<T, 4> = src.nd_view();
+    let dest_strides = NdLayout::contiguous_strides(src_4d.shape());
+
+    struct Dim {
+        size: usize,
+        src_stride: usize,
+        dest_stride: usize,
+    }
+
+    let mut dims = [0, 1, 2, 3].map(|d| Dim {
+        size: src_4d.size(d),
+        src_stride: src_4d.stride(d),
+        dest_stride: dest_strides[d],
     });
+
+    // Sort dims in order of decreasing source stride. This makes the traversal
+    // order over the source as contiguous as possible, which is important for
+    // performance.
+    dims.sort_by_key(|dim| dim.src_stride);
+    dims.reverse();
+
+    // Sanity check before unsafe code below.
+    assert!(src_data.len() >= src_4d.layout().min_data_len());
+
+    for i0 in 0..dims[0].size {
+        for i1 in 0..dims[1].size {
+            for i2 in 0..dims[2].size {
+                for i3 in 0..dims[3].size {
+                    let src_offset = i0 * dims[0].src_stride
+                        + i1 * dims[1].src_stride
+                        + i2 * dims[2].src_stride
+                        + i3 * dims[3].src_stride;
+                    let dest_offset = i0 * dims[0].dest_stride
+                        + i1 * dims[1].dest_stride
+                        + i2 * dims[2].dest_stride
+                        + i3 * dims[3].dest_stride;
+
+                    // Safety: We checked data length > max offset produced
+                    // by layout.
+                    let elt = unsafe { src_data.get_unchecked(src_offset) };
+                    unsafe {
+                        *data.get_unchecked_mut(dest_offset) = elt.clone();
+                    };
+                }
+            }
+        }
+    }
 
     // Safety: Length here matches capacity passed to `Vec::with_capacity`.
     unsafe { data.set_len(src_len) }
