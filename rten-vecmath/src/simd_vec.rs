9012d41cfacf751d1cb1c378f10b038072a0d415
Robert Knight <robertknight@gmail.com>
2024-03-19 17:58:09 +0000

    Inline everything in rten_vecmath::simd_vec
    When attempting to use the `SimdFloat` trait outside of the `rten_vecmath` crate
    I encountered a situation where performance was very poor due to functions not
    being inlined. The latest Rust releases should be inlining small functions
    automatically, but I believe this may not happen when the target also has target
    feature annotations. To keep things simple, just mark all the methods that wrap
    intrinsics as `#[inline]`.

--- rten-vecmath/src/simd_vec.rs
+++ rten-vecmath/src/simd_vec.rs
@@ -38,6 +38,7 @@ pub trait SimdInt: Copy + Sized {
     type Mask: Copy;
 
     /// Return a new vector with all elements set to zero.
+    #[inline]
     unsafe fn zero() -> Self {
         Self::splat(0)
     }
@@ -103,21 +104,25 @@ pub trait SimdFloat: Copy + Sized {
     type Mask: Copy;
 
     /// Shorthand for `Self::splat(1.0)`.
+    #[inline]
     unsafe fn one() -> Self {
         Self::splat(1.0)
     }
 
     /// Shorthand for `Self::splat(0.0)`.
+    #[inline]
     unsafe fn zero() -> Self {
         Self::splat(0.0)
     }
 
     /// Compute `-self`.
+    #[inline]
     unsafe fn neg(self) -> Self {
         Self::zero().sub(self)
     }
 
     /// Compute `1. / self`.
+    #[inline]
     unsafe fn reciprocal(self) -> Self {
         Self::one().div(self)
     }
@@ -167,6 +172,7 @@ pub trait SimdFloat: Copy + Sized {
     /// Evaluate a polynomial using Horner's method.
     ///
     /// Computes `self * coeffs[0] + self^2 * coeffs[1] ... self^n * coeffs[N]`
+    #[inline]
     unsafe fn poly_eval(self, coeffs: &[Self]) -> Self {
         let mut y = coeffs[coeffs.len() - 1];
         for i in (0..coeffs.len() - 1).rev() {
@@ -193,6 +199,7 @@ pub trait SimdFloat: Copy + Sized {
 
     /// Reduce the elements in this vector to a single value using `f`, then
     /// return a new vector with the accumulated value broadcast to each lane.
+    #[inline]
     unsafe fn fold_splat<F: Fn(f32, f32) -> f32>(self, accum: f32, f: F) -> Self {
         let mut elements = [accum; MAX_LEN];
         self.store(elements.as_mut_ptr());
@@ -208,18 +215,22 @@ impl SimdInt for i32 {
     type Float = f32;
     type Mask = bool;
 
+    #[inline]
     unsafe fn zero() -> Self {
         0
     }
 
+    #[inline]
     unsafe fn splat(val: i32) -> Self {
         val
     }
 
+    #[inline]
     unsafe fn gt(self, other: Self) -> Self::Mask {
         self > other
     }
 
+    #[inline]
     unsafe fn blend(self, other: Self, mask: Self::Mask) -> Self {
         if !mask {
             self
@@ -228,26 +239,32 @@ impl SimdInt for i32 {
         }
     }
 
+    #[inline]
     unsafe fn add(self, rhs: Self) -> Self {
         self + rhs
     }
 
+    #[inline]
     unsafe fn sub(self, rhs: Self) -> Self {
         self - rhs
     }
 
+    #[inline]
     unsafe fn shl<const COUNT: i32>(self) -> Self {
         self << COUNT
     }
 
+    #[inline]
     unsafe fn reinterpret_as_float(self) -> Self::Float {
         f32::from_bits(self as u32)
     }
 
+    #[inline]
     unsafe fn load(ptr: *const i32) -> Self {
         *ptr
     }
 
+    #[inline]
     unsafe fn store(self, ptr: *mut i32) {
         *ptr = self;
     }
@@ -260,62 +277,77 @@ impl SimdFloat for f32 {
     type Int = i32;
     type Mask = bool;
 
+    #[inline]
     unsafe fn one() -> Self {
         1.
     }
 
+    #[inline]
     unsafe fn zero() -> Self {
         0.
     }
 
+    #[inline]
     unsafe fn splat(val: f32) -> Self {
         val
     }
 
+    #[inline]
     unsafe fn abs(self) -> Self {
         self.abs()
     }
 
+    #[inline]
     unsafe fn mul_add(self, a: Self, b: Self) -> Self {
         (self * a) + b
     }
 
+    #[inline]
     unsafe fn add(self, rhs: Self) -> Self {
         self + rhs
     }
 
+    #[inline]
     unsafe fn sub(self, rhs: Self) -> Self {
         self - rhs
     }
 
+    #[inline]
     unsafe fn to_int_trunc(self) -> Self::Int {
         self as i32
     }
 
+    #[inline]
     unsafe fn mul(self, rhs: Self) -> Self {
         self * rhs
     }
 
+    #[inline]
     unsafe fn div(self, rhs: Self) -> Self {
         self / rhs
     }
 
+    #[inline]
     unsafe fn ge(self, rhs: Self) -> Self::Mask {
         self >= rhs
     }
 
+    #[inline]
     unsafe fn le(self, rhs: Self) -> Self::Mask {
         self <= rhs
     }
 
+    #[inline]
     unsafe fn lt(self, rhs: Self) -> Self::Mask {
         self < rhs
     }
 
+    #[inline]
     unsafe fn max(self, rhs: Self) -> Self {
         f32::max(self, rhs)
     }
 
+    #[inline]
     unsafe fn blend(self, rhs: Self, mask: Self::Mask) -> Self {
         if !mask {
             self
@@ -324,10 +356,12 @@ impl SimdFloat for f32 {
         }
     }
 
+    #[inline]
     unsafe fn load(ptr: *const f32) -> Self {
         *ptr
     }
 
+    #[inline]
     unsafe fn store(self, ptr: *mut f32) {
         *ptr = self;
     }
