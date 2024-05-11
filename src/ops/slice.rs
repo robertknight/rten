bc7320826a0f9841e383676e1751df23b941d15d
Robert Knight <robertknight@gmail.com>
2024-05-11 19:56:09 +0100

    Improve `Slice` operator efficiency
    Change `Slice` op to use `TensorBase::slice_copy_in`. For cases where all steps
    are positive, this uses the same method to copy the data, but improves
    efficiency by allocating from the pool. For cases where one or more slice ranges
    contains negative steps, this is much more efficient.
    
    Together with the previous change to allocate from the pool, this reduces time
    in `Slice` ops in Piper TTS models from ~3.1ms to ~0.9ms.

--- src/ops/slice.rs
+++ src/ops/slice.rs
@@ -57,25 +57,7 @@ pub fn slice<T: Copy>(
 ) -> Result<Tensor<T>, OpError> {
     let ranges = slice_ranges(input.shape(), starts, ends, axes, steps)?;
     let items: Vec<_> = ranges.iter().map(|r| SliceItem::Range(*r)).collect();
-
-    // Fast path for slice ranges supported by `Tensor::slice`. This includes
-    // all ranges except those with a negative step. This benefits from
-    // optimizations that `Tensor::to_tensor` has for slices that are already
-    // contiguous or have a small number of dims.
-    if let Ok(slice_view) = input.try_slice_dyn(items.as_slice()) {
-        return Ok(slice_view.to_tensor());
-    }
-
-    let sliced_shape: Vec<_> = ranges
-        .iter()
-        .copied()
-        .enumerate()
-        .map(|(dim, range)| range.steps(input.size(dim)))
-        .collect();
-    let mut sliced_data = pool.alloc(sliced_shape.iter().product());
-    sliced_data.extend(input.slice_iter(&items).copied());
-
-    Ok(Tensor::from_data(&sliced_shape, sliced_data))
+    Ok(input.slice_copy_in(pool, items.as_slice()))
 }
 
 /// Clip the dimensions of the input tensor specified by `axes` to the ranges
