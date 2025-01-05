12a4907e1509f1b6eb1f2645e51eb6c4eb50d054
Robert Knight <robertknight@gmail.com>
2025-01-05 17:25:31 +0000

    Use more aggressive approach to unrolling in `simd_gemm`
    For most architectures the compiler unrolls the main GEMM loop as requested. For
    AVX-512 however it didn't. To fix this, swap out the unroll macro for one which
    uses a more aggressive method to force unrolling: duplicating the body 4x.
    
    See https://gist.github.com/robertknight/948170d4edb048804105a99cb4831b4b
    for a before/after comparison.

--- src/iter_util.rs
+++ src/iter_util.rs
@@ -134,12 +134,49 @@ impl MaybeParIter for Range<usize> {
     }
 }
 
+/// Unroll a loop 4x.
+///
+/// This is very similar to [`unroll_loop`] but uses a more aggressive approach
+/// to unrolling which only supports a fixed unroll factor. Whereas
+/// `unroll_loop` uses a hint (a `for` loop with a fixed iteration count) which
+/// the compiler follows most of the time, this macro actually duplicates the
+/// body 4x.
+macro_rules! unroll_loop_x4 {
+    ($range:expr, $loop_var:ident, $block:tt) => {
+        let mut n = $range.len();
+        let mut $loop_var = $range.start;
+
+        while n >= 4 {
+            $block;
+            $loop_var += 1;
+            $block;
+            $loop_var += 1;
+            $block;
+            $loop_var += 1;
+            $block;
+            $loop_var += 1;
+            n -= 4;
+        }
+
+        while n > 0 {
+            $block;
+            $loop_var += 1;
+            n -= 1;
+        }
+    };
+}
+
 /// Generate an unrolled loop.
 ///
 /// `$range` is a `Range` specifying the loop start and end. `$loop_var` is the
 /// name of the variable containing the current iteration inside `$block`.
 /// `$factor` should be a constant expression specifying the unroll factor,
 /// typically a small value such as 4 or 8.
+///
+/// This macro generates a "hint" in the form of a `for` loop with a const
+/// iteration count which the compiler follows in most cases. If it doesn't,
+/// and you're sure you still need unrolling, consider [`unroll_loop_x4`]
+/// instead.
 macro_rules! unroll_loop {
     ($range:expr, $loop_var:ident, $factor: expr, $block:tt) => {
         let mut n = $range.len();
@@ -161,7 +198,7 @@ macro_rules! unroll_loop {
 }
 
 #[allow(unused_imports)]
-pub(crate) use unroll_loop;
+pub(crate) use {unroll_loop, unroll_loop_x4};
 
 #[cfg(test)]
 mod tests {
