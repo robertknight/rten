95649b64dc0a5bc94ab1b22aa2b02657f934b144
Robert Knight <robertknight@gmail.com>
2025-03-22 08:47:30 +0000

    Add traits to enable writing SIMD operations that are generic over element type
    Add `Get{Num, Float, SignedInt}Ops` traits which are implemented for supported
    element types and return the `{Num, Float, SignedInt}Ops` impl for that type
    from a given SIMD [`Isa`]. These can be used to create SIMD operations which are
    generic over the element type.

--- rten-simd/src/lib.rs
+++ rten-simd/src/lib.rs
@@ -165,8 +165,8 @@ pub mod isa {
 pub use dispatch::{SimdOp, SimdUnaryOp};
 pub use iter::{Iter, SimdIterable};
 pub use vec::{
-    Elem, Extend, FloatOps, Interleave, Isa, Mask, MaskOps, NarrowSaturate, NumOps, SignedIntOps,
-    Simd,
+    Elem, Extend, FloatOps, GetFloatOps, GetNumOps, GetSignedIntOps, Interleave, Isa, Mask,
+    MaskOps, NarrowSaturate, NumOps, SignedIntOps, Simd,
 };
 pub use writer::SliceWriter;
 
