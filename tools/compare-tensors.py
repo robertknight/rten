8c21f197b3b063b3ffada2f0e95ba29b1b40c639
Robert Knight <robertknight@gmail.com>
2025-04-08 07:16:51 +0100

    Support JSON tensors in `compare-tensors.py`
    Support reading input tensors as JSON rather than binary in
    `compare-tensors.py`. The expected JSON format matches that used by
    rten-tensor's serde serialization. This JSON format is much less space efficient
    than the binary format, but more convenient to generate.

--- tools/compare-tensors.py
+++ tools/compare-tensors.py
@@ -1,9 +1,22 @@
 from argparse import ArgumentParser
+import json
 import sys
 
 import numpy as np
 
 from debug_utils import read_tensor
+
+def read_json_tensor(path: str):
+    """
+    Load a tensor from a JSON file.
+
+    The JSON data format is `{ "data": [elements...], "shape": [dims...] }`.
+    This matches rten-tensor's serde serialization for the `Tensor` type.
+    """
+    with open(path) as tensor_fp:
+        tensor_json = json.load(tensor_fp)
+        return np.array(tensor_json["data"]).reshape(tensor_json["shape"])
+
 
 def main():
     parser = ArgumentParser(description="Compare two binary tensors")
@@ -11,8 +24,15 @@ def main():
     parser.add_argument('tensor_b', help="File containing second_tensor")
     args = parser.parse_args()
 
-    x = read_tensor(args.tensor_a)
-    y = read_tensor(args.tensor_b)
+    if args.tensor_a.endswith(".json"):
+        x = read_json_tensor(args.tensor_a)
+    else:
+        x = read_tensor(args.tensor_a)
+
+    if args.tensor_b.endswith(".json"):
+        y = read_json_tensor(args.tensor_b)
+    else:
+        y = read_tensor(args.tensor_b)
 
     print(f"X shape {x.shape} Y shape {y.shape}")
 
