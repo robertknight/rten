robertknight/rten

commit 73b65d9bd024962b09a328ae7768ecc2ba59d4a5

Apply fusions in a loop until fixed point is reached
Instead of explicitly separating fusions into separate passes in order to
combine them, iterate until a fixed point is reached.

The actual number of passes that result in changes will be the same for many
models with the current set of fusions (2), but this adds flexibility for the
future.

src/optimize.rs
@@ -129,10 +129,12 @@ impl GraphMutator {
 
     /// Iterate over each operator node in the graph and potentially apply a
     /// fusion which combines this node and adjacent nodes.
+    ///
+    /// Returns the number of applied fusions.
     fn apply_fusion<F: Fn(&Self, NodeId, &OperatorNode) -> Option<Fusion>>(
         &mut self,
         create_fusion: F,
-    ) {
+    ) -> usize {
         struct Replacement {
             unfused_ops: Vec<NodeId>,
             fusion: Fusion,
@@ -173,6 +175,8 @@ impl GraphMutator {
             })
             .collect();
 
+        let n_fusions = fusions.len();
+
         for Replacement {
             fusion,
             unfused_ops,
@@ -193,6 +197,8 @@ impl GraphMutator {
                 &fusion.output_ids,
             );
         }
+
+        n_fusions
     }
 
     fn output_ids(&self) -> &[NodeId] {
@@ -303,23 +309,27 @@ impl GraphOptimizer {
         //
         // The ordering is significant as fusions are tried in turn until a
         // match is found.
-        self.apply_fusions(
-            &mut graph_mut,
-            &[
-                &DynFusion(SiluFusion {}.into_visitor()),
-                &DynFusion(SwishFusion {}.into_visitor()),
-                &DynFusion(GeluFusion {}.into_visitor()),
-                &DynFusion(ApproxGeluFusion {}.into_visitor()),
-                &DynFusion(LayerNormalizationFusion {}),
-                &DynFusion(RmsNormalizationFusion {}.into_visitor()),
-                &DynFusion(MatMulAddFusion {}.into_visitor()),
-                &DynFusion(MatMulScaleFusion {}),
-                &DynFusion(AddSoftmaxFusion {}.into_visitor()),
-            ],
-        )?;
+        let fusions: &[&dyn DynFusionVisitor] = &[
+            &DynFusion(SiluFusion {}.into_visitor()),
+            &DynFusion(SwishFusion {}.into_visitor()),
+            &DynFusion(GeluFusion {}.into_visitor()),
+            &DynFusion(ApproxGeluFusion {}.into_visitor()),
+            &DynFusion(LayerNormalizationFusion {}),
+            &DynFusion(RmsNormalizationFusion {}.into_visitor()),
+            &DynFusion(MatMulAddFusion {}.into_visitor()),
+            &DynFusion(MatMulScaleFusion {}),
+            &DynFusion(AddSoftmaxFusion {}.into_visitor()),
+            &DynFusion(TransposeFusion {}),
+        ];
 
-        // Fuse view operations (transpose etc.) with computations.
-        self.apply_fusions(&mut graph_mut, &[&DynFusion(TransposeFusion {})])?;
+        let max_iters = 3;
+        for _ in 0..max_iters {
+            let n_fused_ops = self.apply_fusions(&mut graph_mut, fusions)?;
+            if n_fused_ops == 0 {
+                // We reached a fixed point.
+                break;
+            }
+        }
 
         Ok(graph_mut.finalize_graph())
     }
@@ -403,16 +413,18 @@ impl GraphOptimizer {
     /// This function visits each operator only once, so won't combine multiple
     /// fusions. Fusions that can be combined must be applied in separate
     /// passes.
+    ///
+    /// Returns the number of fusions that were applied.
     fn apply_fusions(
         &self,
         graph: &mut GraphMutator,
         visitors: &[&dyn DynFusionVisitor],
-    ) -> Result<(), OptimizeError> {
+    ) -> Result<usize, OptimizeError> {
         // Create the prepared state once and then re-use it for each operator
         // visited.
         let prepared_state: Vec<_> = visitors.iter().map(|f| f.prepare(graph.graph())).collect();
 
-        graph.apply_fusion(|graph, op_node_id, op_node| {
+        let n_fusions = graph.apply_fusion(|graph, op_node_id, op_node| {
             for (visitor, state) in visitors.iter().zip(&prepared_state) {
                 if let Some(fusion) =
                     visitor.maybe_fuse(state.as_ref(), graph.graph(), op_node_id, op_node)
@@ -423,7 +435,7 @@ impl GraphOptimizer {
             None
         });
 
-        Ok(())
+        Ok(n_fusions)
     }
 }
 

commit 6f00513bf574eaff9ff1835c9394ce674354aaa8

Fix combining of overlapping fusions
A subgraph like:

`Div(MatMul(Transpose(X), Transpose(Y)), Scale)`

Can be fused into a single `TransformInputs(FusedMatMul)` node, first by
fusing Div + MatMul, then fusing the transposes.

This was broken however after commit 73b65d9bd024962b09a328ae7768ecc2ba59d4a5
because the optimizer would visit the MatMul first and fuse MatMul +
Transpose, then when visiting the subsequent Div operator, it would try
to fuse the Div + the same MatMul that was already fused.

To make this work the optimizer needs to:

 - Ensure that once a fusion is selected, any of the included operators are not
   included in future fusions during the same traversal.

 - Walk the operator graph in reverse toplogical order. Since fusion visitors
   expect to visit the last/output operator of the fusion, this makes fusions
   work as expected when there is an overlap (eg.  MatMulScaleFusion and
   TransposeFusion may both attempt to fuse the same MatMul), or when a fusion
   has optional output nodes (eg.  LayerNormalizationFusin can match
   with/without a bias).

To get the reverse topological order, an execution plan is created and reversed.
A consequence of this is that any operators in the graph which are unused by
the plan will be ignored and not fused. This is probably a good thing since it
avoids spending cycles fusing operations that will likely not be used ("likely"
because RTen allows callers to specify arbitrary nodes as inputs and outputs,
although the optimizer doesn't promise to preserve them).

Fixes https://github.com/robertknight/rten/issues/767

src/optimize.rs
@@ -119,14 +119,6 @@ impl GraphMutator {
         self.graph
     }
 
-    /// Iterate over operator nodes and their IDs.
-    fn iter_operators(&self) -> impl Iterator<Item = (NodeId, &OperatorNode)> {
-        self.graph.iter().filter_map(|(node_id, node)| match node {
-            Node::Operator(op) => Some((node_id, op)),
-            _ => None,
-        })
-    }
-
     /// Iterate over each operator node in the graph and potentially apply a
     /// fusion which combines this node and adjacent nodes.
     ///
@@ -140,9 +132,40 @@ impl GraphMutator {
             fusion: Fusion,
         }
 
-        let fusions: Vec<_> = self
-            .iter_operators()
-            .filter_map(|(op_node_id, op_node)| {
+        let mut ops_pending_removal = FxHashSet::default();
+
+        // Get all operators, in the order they will be run.
+        let Ok(mut operators) = self
+            .graph
+            .execution_plan(self.graph.input_ids(), self.graph.output_ids())
+        else {
+            return 0;
+        };
+
+        // Reverse the operator list so that we traverse from outputs towards
+        // inputs. We do this because fusion matchers start from the output node
+        // of the subgraph they match. Sometimes fusions have optional output
+        // steps which mean they can match multiple nodes. For example the layer
+        // normalization fusion can match with and without an `Add(normalized,
+        // bias)` step at the end. In this case we want to visit the optional
+        // operators first, if present, so that it gets included in the fusion.
+        operators.reverse();
+
+        let fusions: Vec<_> = operators
+            .into_iter()
+            .filter_map(|op_node_id| {
+                let op_node = self
+                    .graph
+                    .get_node(op_node_id)
+                    .and_then(|op| op.as_operator())
+                    .unwrap();
+
+                // Don't try and fuse operators that were removed when fusing a
+                // node we visited earlier.
+                if ops_pending_removal.contains(&op_node_id) {
+                    return None;
+                }
+
                 let fusion = create_fusion(self, op_node_id, op_node)?;
 
                 // Check for outputs of intermediate steps in the fused subgraph
@@ -158,6 +181,16 @@ impl GraphMutator {
 
                 let output_ids: Vec<_> = fusion.output_ids.iter().flatten().copied().collect();
                 let unfused_ops = self.graph.execution_plan(&input_ids, &output_ids).unwrap();
+
+                for unfused_op in &unfused_ops {
+                    // Skip this fusion if it includes an operator which was
+                    // fused when visiting an earlier operator.
+                    if ops_pending_removal.contains(unfused_op) {
+                        return None;
+                    }
+                    ops_pending_removal.insert(*unfused_op);
+                }
+
                 let reused_output = find_operator_output_used_outside_subgraph(
                     &self.graph,
                     &self.edges,
@@ -1008,6 +1041,35 @@ mod tests {
             .and_then(|n| n.as_operator())
             .unwrap();
         assert_eq!(fused_op.operator().name(), "Mul");
+    }
+
+    #[test]
+    fn test_fuse_transpose_matmul_scaled() {
+        // Div(MatMul(Transpose(X), Transpose(Y)), C) subgraph.
+        //
+        // This should be fused into a single operation in two stages:
+        //  1. Fuse Div + MatMul into `FusedMatMul(Transpose(X), Transpose(Y), alpha=C)`
+        //  2. Fuse FusedMatMul + Transpose into `TransformInputs(FusedMatMul(X, Y, alpha=C))`.
+        let x = Expr::value("x");
+        let y = Expr::value("y");
+        let xy = x.transpose().matmul(y.transpose());
+        let xy_scaled = xy / 8.;
+        let graph = xy_scaled.build_graph(["x", "y"]);
+
+        let input_ids = graph.input_ids().to_vec();
+        let output_ids = graph.output_ids().to_vec();
+        let optimized = optimize_graph(graph).unwrap();
+        let plan = optimized.execution_plan(&input_ids, &output_ids).unwrap();
+
+        let op_name = |node_id| {
+            optimized
+                .get_node(node_id)
+                .and_then(|n| n.as_operator())
+                .map(|op| op.operator().name())
+        };
+
+        assert_eq!(plan.len(), 1);
+        assert_eq!(op_name(plan[0]), Some("TransformInputs(FusedMatMul)"));
     }
 
     #[test]