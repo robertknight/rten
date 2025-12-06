0022d392abeaad13bd220740db8b52f42d4a5d06
Robert Knight <robertknight@gmail.com>
2025-12-06 18:47:59 +0000

    Add shape inference for Unsqueeze operator

--- rten-shape-inference/src/ops.rs
+++ rten-shape-inference/src/ops.rs
@@ -5,7 +5,7 @@
 
 use crate::infer_shapes::{InferShapes, InferShapesError, resolve_axis};
 use crate::sym_gen::SymbolGen;
-use crate::sym_tensor::SymTensor;
+use crate::sym_tensor::{SymElem, SymTensor};
 
 /// Concat operator.
 ///
@@ -58,12 +58,64 @@ impl InferShapes for Concat {
     }
 }
 
+/// Unsqueeze operator.
+///
+/// See <https://onnx.ai/onnx/operators/onnx__Unsqueeze.html>.
+pub struct Unsqueeze;
+
+impl InferShapes for Unsqueeze {
+    fn infer_shapes(
+        &self,
+        inputs: &[SymTensor],
+        _sym_gen: &mut SymbolGen,
+    ) -> Result<Vec<SymTensor>, InferShapesError> {
+        let [data, axes] = inputs else {
+            return Err(InferShapesError::IncorrectInputCount);
+        };
+
+        // If data is a constant or symbolic scalar and axes is 0, the output
+        // will be a length-1 vector. We can't handle higher-rank symbolic
+        // values yet.
+        //
+        // Otherwise if the input shape is known and `axes` is a constant, the
+        // output is a symbolic shape.
+        //
+        // Otherwise the output is unknown.
+
+        let axes_vec = axes.to_constant().map(|c| c.into_vec());
+
+        let value = if let Some(var) = data.as_scalar()
+            && axes_vec.as_deref().map(|v| v == [0]).unwrap_or(false)
+        {
+            SymTensor::from_vec([var.clone()].into())
+        } else if let Some(dims) = data.shape()
+            && let Some(mut axes) = axes_vec
+        {
+            let mut dims: Vec<_> = dims.collect();
+            axes.sort();
+
+            for (i, axis) in axes.into_iter().enumerate() {
+                let axis = resolve_axis(dims.len() + 1, axis)
+                    .map_err(|_| InferShapesError::IncorrectRank)?;
+                dims.insert(axis + i, SymElem::Value(1));
+            }
+
+            SymTensor::from_shape(dims)
+        } else {
+            SymTensor::unknown("unknown data shape or axes value")
+        };
+
+        Ok([value].into())
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::Concat;
     use crate::infer_shapes::InferShapes;
     use crate::sym_gen::SymbolGen;
     use crate::sym_tensor::{SymElem, SymTensor, sym_elems, sym_shape, sym_vec};
+
+    use super::{Concat, Unsqueeze};
 
     fn extract_shape(mut result: Vec<SymTensor>) -> Vec<SymElem> {
         result.remove(0).shape().unwrap().collect()
@@ -105,5 +157,29 @@ mod tests {
             result.remove(0).as_vector().unwrap(),
             sym_elems!("batch", "chans", "height", "width")
         );
+    }
+
+    #[test]
+    fn test_unsqueeze() {
+        let mut sym_gen = SymbolGen::new();
+
+        // Unsqueeze into an ND-tensor.
+        let shape = sym_shape!("batch", 16, 64);
+        let axes = sym_vec!(1);
+        let expected = sym_shape!("batch", 1, 16, 64);
+
+        let result = Unsqueeze
+            .infer_shapes(&[shape, axes], &mut sym_gen)
+            .unwrap();
+        assert_eq!(result[0], expected);
+
+        // Unsqueeze a symbolic scalar into a symbolic vec.
+        let scalar = SymTensor::from_scalar(1.into());
+        let axes = sym_vec!(0);
+        let expected = sym_vec!(1);
+        let result = Unsqueeze
+            .infer_shapes(&[scalar, axes], &mut sym_gen)
+            .unwrap();
+        assert_eq!(result[0], expected);
     }
 }
