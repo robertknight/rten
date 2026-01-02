a8f6a68a63eec94c3718108caec57c745b883b8a
Robert Knight <robertknight@gmail.com>
2026-01-02 11:17:39 +0000

    Extract ComputeShape operator to its own module

--- src/ops/layout.rs
+++ src/ops/layout.rs
@@ -747,67 +747,6 @@ impl Operator for Unsqueeze {
     }
 }
 
-/// Specifies the source for a tensor shape computed by [`ComputeShape`].
-#[derive(Debug, Clone, PartialEq)]
-pub enum DimSpec {
-    /// Output a fixed value.
-    Static(u32),
-    /// Copy the size of a tensor dimension from an input.
-    Dynamic { input: u32, dim: u32 },
-}
-
-/// Compute a tensor shape from a combination of static values and the dynamic
-/// shapes of inputs.
-///
-/// This is a custom internal operator produced by fusions.
-#[derive(Debug)]
-pub struct ComputeShape {
-    /// Specifies the length of the output vector and how to compute each element.
-    pub shape: Vec<DimSpec>,
-}
-
-impl Operator for ComputeShape {
-    fn name(&self) -> &str {
-        "ComputeShape"
-    }
-
-    fn max_inputs(&self) -> Option<usize> {
-        None
-    }
-
-    fn run(&self, ctx: &OpRunContext) -> Result<OutputList, OpError> {
-        let inputs = ctx.inputs();
-
-        let output = self
-            .shape
-            .iter()
-            .map(|dim| match dim {
-                DimSpec::Static(size) => Ok(*size as i32),
-                DimSpec::Dynamic {
-                    input: input_idx,
-                    dim,
-                } => {
-                    let dim = *dim as usize;
-                    let input = inputs.require(*input_idx as usize)?;
-                    if input.ndim() > dim {
-                        Ok(input.size(dim).min(i32::MAX as usize) as i32)
-                    } else {
-                        Err(OpError::InvalidValue(
-                            "Dim index invalid for input tensor shape",
-                        ))
-                    }
-                }
-            })
-            .collect::<Result<Vec<i32>, _>>()?;
-
-        Tensor::from(output).into_op_result()
-    }
-
-    fn output_types(&self, _ctx: &OutputTypesContext) -> Option<OutputTypeList> {
-        Some([OutputType::Fixed(ValueType::Tensor(DataType::Int32))].into())
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use std::error::Error;
@@ -819,7 +758,7 @@ mod tests {
     use rten_tensor::{NdTensor, Tensor};
     use rten_testing::TestCases;
 
-    use super::{ComputeShape, DepthToSpaceMode, DimSpec, depth_to_space};
+    use super::{DepthToSpaceMode, depth_to_space};
     use crate::buffer_pool::BufferPool;
     use crate::operator::{OpError, OperatorExt};
     use crate::ops::layout::{
@@ -827,43 +766,6 @@ mod tests {
         squeeze_in_place, transpose, unsqueeze,
     };
     use crate::value::Value;
-
-    #[test]
-    fn test_compute_shape() {
-        // Valid input with a mix of static and dynamic output values.
-        let input_a = NdTensor::<f32, _>::zeros([2, 4, 8]);
-        let input_b = NdTensor::<f32, _>::zeros([24]);
-
-        let op = ComputeShape {
-            shape: [
-                DimSpec::Static(3),
-                DimSpec::Dynamic { input: 0, dim: 1 },
-                DimSpec::Static(5),
-                DimSpec::Dynamic { input: 1, dim: 0 },
-            ]
-            .into(),
-        };
-        let result: NdTensor<i32, 1> = op.run_simple((input_a.view(), input_b.view())).unwrap();
-
-        assert_eq!(result, NdTensorView::from(&[3, 4, 5, 24]));
-
-        // Dynamic input with invalid input index.
-        let op = ComputeShape {
-            shape: [DimSpec::Dynamic { input: 1, dim: 0 }].into(),
-        };
-        let result: Result<NdTensor<i32, 1>, _> = op.run_simple(input_a.view());
-        assert_eq!(result.err().unwrap(), OpError::MissingInputs);
-
-        // Dynamic input with invalid dim index.
-        let op = ComputeShape {
-            shape: [DimSpec::Dynamic { input: 0, dim: 3 }].into(),
-        };
-        let result: Result<NdTensor<i32, 1>, _> = op.run_simple(input_a.view());
-        assert_eq!(
-            result.err().unwrap(),
-            OpError::InvalidValue("Dim index invalid for input tensor shape")
-        );
-    }
 
     #[test]
     fn test_depth_to_space() {
