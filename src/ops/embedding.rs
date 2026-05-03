3e761f643d1eb0df18b7e8eae04fc9bcf0020441
xd009642 <danielmckenna93@gmail.com>
2026-05-03 10:57:28 +0900

    Apply suggested diff

--- src/ops/embedding.rs
+++ src/ops/embedding.rs
@@ -1,9 +1,9 @@
 use rten_tensor::{AsView, Layout, NdTensorView, Tensor, TensorView};
 
-use crate::value::Value;
 use crate::{
+    buffer_pool::AutoReturn,
     operator::{
-        IntoOpResult, OpError, OpRunContext, Operator, OutputList, OutputTypeList,
+        IntoOpResult, OpError, OpRunContext, Operator, OutputList, OutputType, OutputTypeList,
         OutputTypesContext,
     },
     ops::{
@@ -72,22 +72,12 @@ impl Operator for RotaryEmbedding {
 
         let rotary_embedding_dim_half = rotary_embedding_dim / 2;
 
-        // TODO this is a bit ugly, just doing it to coerce the subsets into the same type as cos
-        // and sin if no position indices are given with the data living long enough
-        let mut cos_subset = None;
-        let mut sin_subset = None;
-
         let (cos_cache, sin_cache) = if let Some(position_ids) = position_ids {
-            cos_subset = Some(gather(ctx.pool(), cos, 0, position_ids.as_dyn())?);
-            sin_subset = Some(gather(ctx.pool(), sin, 0, position_ids.as_dyn())?);
-
-            // Ugly unwrap
-            (
-                cos_subset.as_ref().map(|x| x.view()).unwrap(),
-                sin_subset.as_ref().map(|x| x.view()).unwrap(),
-            )
+            let cos_subset = gather(ctx.pool(), cos, 0, position_ids.as_dyn())?.into_cow();
+            let sin_subset = gather(ctx.pool(), sin, 0, position_ids.as_dyn())?.into_cow();
+            (cos_subset, sin_subset)
         } else {
-            (cos, sin)
+            (cos.as_cow(), sin.as_cow())
         };
 
         if cos_cache.shape()[cos_cache.ndim() - 1] != rotary_embedding_dim_half {
@@ -102,8 +92,8 @@ impl Operator for RotaryEmbedding {
             ));
         }
 
-        let cos_cache = cos_cache.with_new_axis(2);
-        let sin_cache = sin_cache.with_new_axis(2);
+        let cos_cache = cos_cache.view().with_new_axis(2);
+        let sin_cache = sin_cache.view().with_new_axis(2);
 
         let (x1, x2) = if self.interleaved != 0 {
             (
@@ -114,51 +104,43 @@ impl Operator for RotaryEmbedding {
             x_rotate.split_at(3, rotary_embedding_dim_half)
         };
 
-        // TODO maybe inplace version is possible and nicer
-        let lhs = mul(ctx.pool(), cos_cache.view(), x1.as_dyn())?;
-        let rhs = mul(ctx.pool(), sin_cache.view(), x2.as_dyn())?;
-        let real = sub(ctx.pool(), lhs.view(), rhs.view())?;
+        let lhs = mul(ctx.pool(), cos_cache.view(), x1.as_dyn())?.auto_return(ctx.pool());
+        let rhs = mul(ctx.pool(), sin_cache.view(), x2.as_dyn())?.auto_return(ctx.pool());
+        let real = sub(ctx.pool(), lhs.view(), rhs.view())?.auto_return(ctx.pool());
 
-        let lhs = mul(ctx.pool(), sin_cache.view(), x1.as_dyn())?;
-        let rhs = mul(ctx.pool(), cos_cache.view(), x2.as_dyn())?;
-        let imag = add(ctx.pool(), lhs.view(), rhs.view())?;
+        let lhs = mul(ctx.pool(), sin_cache.view(), x1.as_dyn())?.auto_return(ctx.pool());
+        let rhs = mul(ctx.pool(), cos_cache.view(), x2.as_dyn())?.auto_return(ctx.pool());
+        let imag = add(ctx.pool(), lhs.view(), rhs.view())?.auto_return(ctx.pool());
 
         let x_rotate = if self.interleaved != 0 {
             let insert_axis = real.ndim() - 1;
-            let real = real.with_new_axis(insert_axis);
-            let imag = imag.with_new_axis(insert_axis);
+            let real = real.view().with_new_axis(insert_axis);
+            let imag = imag.view().with_new_axis(insert_axis);
 
-            let mut x_rotate_concat = concat(ctx.pool(), &[real.view(), imag.view()], -1)?;
+            let mut x_rotate_concat = concat(ctx.pool(), &[real, imag], -1)?;
             x_rotate_concat.reshape(&x_rotate.shape());
             x_rotate_concat
         } else {
             concat(ctx.pool(), &[real.view(), imag.view()], -1)?
-        };
+        }
+        .auto_return(ctx.pool());
 
         let output = concat(ctx.pool(), &[x_rotate.view(), x_not_rotate.as_dyn()], -1)?;
 
         let output = if input.ndim() == 3 {
-            output.reshaped(input.shape())
+            output.into_shape(input.shape())
         } else {
-            input.permuted(&[0, 2, 1, 3]).as_cow()
+            input.permuted(&[0, 2, 1, 3]).to_tensor_in(ctx.pool())
         };
 
-        output.to_tensor().into_op_result()
+        output.into_op_result()
     }
 
     fn is_commutative(&self) -> bool {
         false
     }
 
-    fn can_run_in_place(&self) -> bool {
-        false
-    }
-
-    fn run_in_place(&self, input: Value, ctx: &OpRunContext) -> Result<Value, OpError> {
-        todo!()
-    }
-
     fn output_types(&self, _ctx: &OutputTypesContext) -> Option<OutputTypeList> {
-        todo!()
+        Some([OutputType::CopyFromInput(0)].into())
     }
 }
